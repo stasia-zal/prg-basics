8f68b5ac2ccd421cc22966435011884cd7308ec8
Anastasiia Zaluzhna <nastyazaluzhna@gmail.com>
2025-10-23 20:00:09 +0200

    done most

---
+++ 02-TypesAndVariables/fString.py
@@ -0,0 +1,9 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+height=184
+age=17
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age+6} years old.  ")
---
+++ 02-TypesAndVariables/horizon.py
@@ -0,0 +1,12 @@
+
+import math
+
+h=0
+d = 3.57 * (math.sqrt(h))
+
+print (d)
+
+h=20
+d=3.57 * (math.sqrt(h))
+
+print (d)
